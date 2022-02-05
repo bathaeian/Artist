2170a6ce23b4ef1529951f803d514b6351f69ebe
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2022-02-05 14:29:30 +0330

    Add files via upload

--- ArtArea.cpp
+++ ArtArea.cpp
@@ -1,47 +1,52 @@
-#include "ArtArea.h"
+#include <gtkmm.h>
 #include <cairomm/context.h>
 #include <gdkmm/general.h> // set_source_pixbuf()
 #include <glibmm/fileutils.h>
 #include <iostream>
+#include <gdkmm/pixbuf.h>
+#include <glibmm/main.h>
+
+class ArtArea : public Gtk::DrawingArea
+{
+public:
+  ArtArea();
+  void incColor();
+  void setColors(int , int ,int);
+protected:
+  virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);//, int width, int height);
+  bool on_button_press_event(GdkEventButton *);
+  Glib::RefPtr<Gdk::Pixbuf> m_image;
+  private:
+     int ncolor;
+     int x1,y1,x2,y2;
+     bool firstclick,secondclick;
+     int red,green,blue;
+};
 
 ArtArea::ArtArea()
 {
     m_image = Gdk::Pixbuf::create_from_file("art.png");
-   // Show at least a quarter of the image.
-  if (m_image)
-  {
-    set_content_width(m_image->get_width()/2);
-    set_content_height(m_image->get_height()/2);
-  }
-
-  // Set the draw function.
-  set_draw_func(sigc::mem_fun(*this, &MyArea::on_draw));
-  add_events(Gdk::BUTTON_PRESS_MASK);
-    //startvalues
+    add_events(Gdk::BUTTON_PRESS_MASK);
+    ncolor=0;
     firstclick=false;
     secondclick=false;
+    red=green=blue=0;
 }
 
-void ArtArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
+bool ArtArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 {
   if (!m_image)
-    return;
-
-
-  // Draw the image in the middle of the drawing area, or (if the image is
-  // larger than the drawing area) draw the middle part of the image.
+    return false;
   if(ncolor>3){
-    Gdk::Cairo::set_source_pixbuf(cr, m_image,
-        (width - m_image->get_width())/2, (height - m_image->get_height())/2);
-      cr->paint();
+    Gdk::Cairo::set_source_pixbuf(cr, m_image);
+    cr->paint();
   }
-
   else if(firstclick&&secondclick)
     {
         //set the width of the line
         cr->set_line_width(2);
         //set the color: black
-        cr->set_source_rgb(0,0,0);
+        cr->set_source_rgb(red,green,blue);
         //move the "brush" to the first point
         cr->move_to(x1,y1);
         //draw the line to the second point
@@ -51,7 +56,7 @@ void ArtArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int he
         firstclick=false;
         secondclick=false;
     }
-
+  return true;
 }
 void ArtArea::incColor(){ncolor++;}
 bool ArtArea::on_button_press_event(GdkEventButton *event)
@@ -80,4 +85,8 @@ bool ArtArea::on_button_press_event(GdkEventButton *event)
         // The event has been handled.
         return true;
     }
+    return false;
+}
+void ArtArea::setColors(int r,int g , int b){
+  red=r;green=g;blue=b;
 }
